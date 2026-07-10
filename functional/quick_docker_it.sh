d6e860c82a94d6ee5164c019ff5d74c139ac8bd6
mdaghlian <marcusdag@googlemail.com>
2026-07-10 17:52:57 +0100

    Add quick_docker_it.sh script for running Docker with BIDS and FreeSurfer configurations

---
+++ functional/quick_docker_it.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run --rm -it \
+    -e BIDS_DIR=/data/ \
+    -e SUBJECTS_DIR=/data//derivatives/freesurfer \
+    -e FS_LICENSE=/opt/freesurfer/license.txt \
+    -v $BIDS_DIR:/data \
+    -v $FSLICENSE:/opt/freesurfer/license.txt \
+    $1 
